a70213605e140b4e0e56ffab13eaf8eb7ba4ae15
JamesHughes <jhughes1790@gmail.com>
2022-04-16 12:03:47 -0400

    workshop 2, task 1, unfinished

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+
+
+
+
+
+export default CampsiteInfo;
